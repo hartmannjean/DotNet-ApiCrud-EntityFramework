c47e70371a319e73b0294dc68515f75bd69c64dd
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2024-11-06 19:41:58 -0300

    Added comment about DTO

--- Estudantes/EstudanteDto.cs
+++ Estudantes/EstudanteDto.cs
@@ -1,3 +1,9 @@
 ﻿namespace ApiCrud.Estudantes;
-    public record EstudanteDto(Guid Id, string Nome);
+/*
+ O DTO (Data Transfer Object) é um objeto que carrega dados entre processos.
+    Ele é usado para minimizar o número de chamadas entre aplicativos e servidores.
+    O DTO é um objeto simples que não contém lógica de negócios, mas apenas dados.
+    Quando voce não quer expor a estrutura interna de seus objetos, você pode usar DTOs.
+ */
+public record EstudanteDto(Guid Id, string Nome);
 
