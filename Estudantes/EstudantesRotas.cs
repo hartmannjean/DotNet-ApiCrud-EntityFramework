1c12507d77c9a18e43d10a3895155f2c1b7acc0f
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2024-11-06 19:21:37 -0300

    Added CancellationToken into routes

--- Estudantes/EstudantesRotas.cs
+++ Estudantes/EstudantesRotas.cs
@@ -10,17 +10,17 @@ namespace ApiCrud.Estudantes {
 
             //Add novo estudante
             rotasestudantes.MapPost("",
-                async (AddEstudanteRequest request, AppDbContext context) =>
+                async (AddEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
                 {
                     var jaExiste = await context.Estudantes
-                    .AnyAsync(estudante => estudante.Nome == request.Nome);
+                    .AnyAsync(estudante => estudante.Nome == request.Nome, ct);
 
                     if (jaExiste)
                         return Results.Conflict("Já existe!");
 
                     var novoEstudante = new Estudante(request.Nome);
-                    await context.Estudantes.AddAsync(novoEstudante);
-                    await context.SaveChangesAsync();
+                    await context.Estudantes.AddAsync(novoEstudante, ct);
+                    await context.SaveChangesAsync(ct);
 
                     var estudanteRetorno = new EstudanteDto(novoEstudante.Id, novoEstudante.Nome);
                     return Results.Ok(estudanteRetorno);
@@ -29,41 +29,41 @@ namespace ApiCrud.Estudantes {
 
             //Pegar todos os estudantes
             rotasestudantes.MapGet("",
-                async (AppDbContext context) =>
+                async (AppDbContext context, CancellationToken ct) =>
                 {
                     var estudantes = await context
                         .Estudantes
                         .Where(estudante => estudante.Ativo)
                         .Select(estudante => new EstudanteDto(estudante.Id, estudante.Nome))
-                        .ToListAsync();
+                        .ToListAsync(ct);
 
                     return Results.Ok(estudantes);
                 });
 
             //Atualizar nome dos estudantes
             rotasestudantes.MapPut("{id:guid}", 
-                async (Guid id, UpdateEstudanteRequest request, AppDbContext context) =>
+                async (Guid id, UpdateEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
                 {
-                    var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id);
+                    var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id, ct);
                     if (estudante == null)
                         return Results.NotFound();
                     
                     estudante.AtualizarNome(request.nome);
 
-                    await context.SaveChangesAsync();
+                    await context.SaveChangesAsync(ct);
                     return Results.Ok(new EstudanteDto(estudante.Id, estudante.Nome));
                 });
 
             //Deletar um registro - soft delete(inativa o registro)
             rotasestudantes.MapDelete("{id}",
-                async (Guid id, AppDbContext context) =>
+                async (Guid id, AppDbContext context, CancellationToken ct) =>
                 {
-                    var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id);
+                    var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id, ct);
                     if (estudante == null)
                         return Results.NotFound();
 
                     estudante.DesativarEstudante();
-                    await context.SaveChangesAsync();
+                    await context.SaveChangesAsync(ct);
                     return Results.Ok();
 
                    
