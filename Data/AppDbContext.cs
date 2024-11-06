651e2f09d3b6dd151ec168af0fdbd3ec2447d854
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2024-11-06 19:44:01 -0300

    Added comment about DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,6 +2,12 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace ApiCrud.Data {
+
+    /*
+        DbContext é a classe principal do Entity Framework Core que é usada para se comunicar com o banco de dados.
+        Ele é responsável por configurar o modelo, rastrear as entidades, executar consultas e salvar alterações no banco de dados.
+        O DbContext é uma combinação de Unit of Work e Repository Patterns
+     */
     public class AppDbContext : DbContext
     {
         public DbSet<Estudante> Estudantes { get; set; }
